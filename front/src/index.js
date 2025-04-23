6b815cbcf2dd5681262a43c5cb54318890a8e3a3
sm1leProgrammer <petaevd@yandex.ru>
2025-04-23 14:10:33 +0300

    refactor

--- front/src/index.js
+++ front/src/index.js
@@ -1,14 +1,12 @@
 import React from 'react';
-import { createRoot } from 'react-dom/client'; // Импортируем createRoot
+import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import store from './store';
 import App from './App';
 
-// 1. Создаем корневой элемент
 const container = document.getElementById('root');
 const root = createRoot(container);
 
-// 2. Рендерим приложение с Redux Provider
 root.render(
   <React.StrictMode>
     <Provider store={store}>
