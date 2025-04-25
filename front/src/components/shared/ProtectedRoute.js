5d19baa2d7524a7555b07011cfe2b726b6d48209
sm1leProgrammer <petaevd@yandex.ru>
2025-04-25 15:58:54 +0300

    удалил user

--- front/src/components/shared/ProtectedRoute.js
+++ front/src/components/shared/ProtectedRoute.js
@@ -2,7 +2,7 @@ import { Navigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 
 const ProtectedRoute = ({ children }) => {
-  const { isAuthenticated, token, user } = useSelector((state) => state.auth);
+  const { isAuthenticated, token } = useSelector((state) => state.auth);
   const storedToken = localStorage.getItem('token');
 
   if (!isAuthenticated || !token || !storedToken) {
